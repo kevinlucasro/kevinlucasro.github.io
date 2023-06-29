bbf0cc288468d7132048e48f17a29141e3ebd8f9
Kevin Lucas <116270915+kevinlucasro@users.noreply.github.com>
2023-06-29 14:28:25 -0300

    up

---
+++ js.js
@@ -0,0 +1,11 @@
+function desvia(t) {
+    var btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log("opa, desviei...");
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
